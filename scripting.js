ea07368151551683e35f2dc70b44ace9a5541981
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2022-10-20 14:53:58 -0700

    added round tracking and player and computer score tracking

--- scripting.js
+++ scripting.js
@@ -6,6 +6,14 @@ const rock_button = document.querySelector('#rock');
 const paper_button = document.querySelector('#paper');
 const scissors_button = document.querySelector('#scissors');
 
+const round_label = document.querySelector('#round');
+const player_score_label = document.querySelector('#player_score');
+const computer_score_label = document.querySelector('#computer_score');
+
+let round = 1;
+let player_score=0;
+let computer_score=0;
+
 function getComputerChoice() {
     // Math.floor returns the greatest integer <= its argument
     let randomInt=Math.floor(Math.random() * 3);
@@ -37,7 +45,7 @@ function playRound(player_selection) {
         resultlabel.textContent=`Tie! ${player_selection.charAt(0).toUpperCase()+player_selection.slice(1)} ties with ${computer_selection}!`
     } else {
         if (player_selection == 'rock' && computer_selection == 'paper') {
-            resultlabel.textContent='You Lose! Rock loses to paper!';
+            resultlabel.textContent='You lose! Rock loses to paper!';
         } else if (player_selection == 'rock' && computer_selection == 'scissors') {
             resultlabel.textContent='You win! Rock beats scissors!';
         } else if (player_selection == 'scissors' && computer_selection == 'rock') {
@@ -50,6 +58,19 @@ function playRound(player_selection) {
             resultlabel.textContent='You lose! Paper loses to scissors!';
         }
     }
+
+    // update score and round number
+    if (resultlabel.textContent.includes('You win')) {
+        player_score++;
+        player_score_label.textContent=`Player score: ${player_score}`;
+    } else if (resultlabel.textContent.includes('You lose')) {
+        computer_score++;
+        computer_score_label.textContent=`Computer score: ${computer_score}`;
+    }
+
+    round++;
+    round_label.textContent = `Round ${round}`
+
 }
 
 rock_button.addEventListener('click',() => {playRound('rock')});
